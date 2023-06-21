2e712f97754dc86ee85675df19ef0e340d41db8b
Laurent Querel <laurent.querel@gmail.com>
2023-06-20 17:36:23 -0700

    Update error msg

--- src/errors.rs
+++ src/errors.rs
@@ -1,6 +1,6 @@
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
-    #[error("invalid cwe file {file}: {error}")]
+    #[error("invalid CWE file {file}: {error}")]
     InvalidCweFile {
         file: String,
         error: String,
